ff948f53ef2d49bf5b88e59f0c5acff3d2d3ccd7
riteshkharat07 <riteshkharat07@gmail.com>
2026-01-19 19:21:43 +0530

    Create VariablesAndDataTypes.java

---
+++ day-02/VariablesAndDataTypes.java
@@ -0,0 +1,31 @@
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+
+        // Integer type
+        int age = 21;
+
+        // Floating point type
+        float percentage = 85.5f;
+
+        // Double type
+        double salary = 45000.75;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // String type
+        String name = "Ritesh";
+
+        // Printing values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
