a2f2e448cae4ccec865cfb38679a169b7b660f9d
riteshkharat07 <riteshkharat07@gmail.com>
2026-01-24 18:30:35 +0530

    Day 06: for, while and do-while loops in Java

---
+++ day-06/LoopsExample.java
@@ -0,0 +1,27 @@
+public class LoopsExample {
+
+    public static void main(String[] args) {
+
+        // -------- for loop --------
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // -------- while loop --------
+        System.out.println("\nWhile Loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+
+        // -------- do-while loop --------
+        System.out.println("\nDo-While Loop:");
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
+    }
+}
