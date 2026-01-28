7d3c1a610abcdeb9b55828149d14d03d50dd24de
riteshkharat07 <riteshkharat07@gmail.com>
2026-01-28 12:50:29 +0530

    Create TwoDArrayBasics.java

---
+++ day-04-arrays/TwoDArrayBasics.java
@@ -0,0 +1,10 @@
+public class TwoDArrayBasics {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        System.out.println(matrix[0][0]); // 1
+        System.out.println(matrix[1][2]); // 6
